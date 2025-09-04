5bdc86406b6e9db9816ba19e4ef533296c166a99
Simon Hartcher <simon@simonhartcher.com>
2025-09-04 16:47:31 +1000

    feat: build bchunk with zig + add gh workflow

---
+++ build.zig
@@ -0,0 +1,36 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const bchunk = b.dependency("bchunk", .{});
+
+    const bchunk_mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+
+    bchunk_mod.addCSourceFile(.{
+        .file = bchunk.builder.path("bchunk.c"),
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "bchunk",
+        .root_module = bchunk_mod,
+    });
+
+    b.installArtifact(exe);
+
+    const run_step = b.step("run", "Run the app");
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_step.dependOn(&run_cmd.step);
+
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+}
